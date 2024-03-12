a439bfa0a941f24c9e93d76167be5cd8c4661968
Dmitrii Ilichev <dsilichev@gmail.com>
2024-03-13 01:19:13 +0300

    add handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import styles from './app.module.css';
 import data from './data.json';
 
 export const App = () => {
   // Можно задать 2 состояния — steps и activeIndex
-
+  let [steps, setSteps] = useState(data);
+  let [activeIndex, setActiveIndex] = useState(0);
   // И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+  const stepBack = () => {
 
+  }
+
+  const stepForward = () => {
+
+  }
+
+  const startAgain = () => {
+
+  }
   // И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+  let isFirstStep = true;
+  let isLastStep = false;
 
   return (
     <div className={styles.container}>
@@ -15,7 +29,7 @@ export const App = () => {
         <div className={styles.steps}>
           <div className={styles['steps-content']}>
             {/* Для получения активного контента использйте steps и activeIndex */}
-            Контент соответственный шагу. Сейчас активен шаг 3
+            Контент соответственный шагу. Сейчас активен шаг {activeIndex + 1}
           </div>
           <ul className={styles['steps-list']}>
             {/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
@@ -41,8 +55,8 @@ export const App = () => {
             </li>
           </ul>
           <div className={styles['buttons-container']}>
-            <button className={styles.button}>Назад</button>
-            <button className={styles.button}>
+            <button className={styles.button} onClick={stepBack}>Назад</button>
+            <button className={styles.button} onClick={stepForward}>
               Далее
               {/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
               {/* Или заменять всю кнопку в зависимости от условия */}

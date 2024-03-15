b5ec3ff23deaa0e64d1391671aada7ef3db5e918
Dmitrii Ilichev <dsilichev@gmail.com>
2024-03-16 00:41:01 +0300

    click navigation

--- src/App.js
+++ src/App.js
@@ -9,21 +9,19 @@ export const App = () => {
   // И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
   const stepBack = () => {
     setActiveIndex(activeIndex => activeIndex - 1);
-    console.log(isLastStep);
   }
 
   const stepForward = () => {
     setActiveIndex((activeIndex) => activeIndex + 1);
-    if (activeIndex > 0) {
-    }
   }
 
   const startAgain = () => {
     setActiveIndex(0);
   }
-  // И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
-  let isFirstStep = true;
-  let isLastStep = false;
+
+  const chooseStep = (stepNumber) => {
+    setActiveIndex(stepNumber);
+  }
 
   return (
     <div className={styles.container}>
@@ -39,21 +37,18 @@ export const App = () => {
             {steps.map(({ id, title }) =>
               <li key={id} className={styles['steps-item'] + ' ' + (Number(id) <= activeIndex + 1 ? styles.done : '') + ' ' + (Number(id) === activeIndex + 1 ? styles.active : '')}>
                 {/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
-                <button className={styles['steps-item-button']}>{Number(id)}</button>
+                <button onClick={() => chooseStep(Number(id) - 1)} className={styles['steps-item-button']}>{Number(id)}</button>
                 {/* При клике на кнопку установка выбранного шага в качестве активного */}
                 {title}
               </li>
             )}
-
           </ul>
           <div className={styles['buttons-container']}>
             <button className={styles.button} onClick={stepBack} disabled={activeIndex === 0}>Назад</button>
             {activeIndex === steps.length - 1 ? <button className={styles.button} onClick={startAgain}>Начать сначала</button> : <button className={styles.button} onClick={stepForward}>
               Далее
-              {/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-              {/* Или заменять всю кнопку в зависимости от условия */}
             </button>}
-            
+
           </div>
         </div>
       </div>
